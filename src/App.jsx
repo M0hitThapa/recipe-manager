062695cf8580fd925a346da60cbfa3a7a2a76dd8
Mohit Thapa <thapamohit2058@gmail.com>
2025-07-12 15:26:43 +0545

    recipe component make

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import RecipeList from './RecipeList'
 
 function App() {
   return (
-    <div>App</div>
+    <RecipeList recipes={sampleRecipes} />
   )
 }
+
+const sampleRecipes = [
+  {
+    id:1,
+    name:"Plain Chicken",
+    servings:3,
+    cookTime:"1:45",
+    instructions:"1. Put Salt on chicken\n2  2.Put chichen in oven\n3 3. eat chicken"
+
+  },
+  {
+    id:2,
+    name:"Plain Chi3cken",
+    servings:3,
+    cookTime:"1:25",
+    instructions:"1. Put Salt on chicken\n2  2.Put chichen in oven\n3 3. eat chicken"
+
+  }
+]
 
 export default App
